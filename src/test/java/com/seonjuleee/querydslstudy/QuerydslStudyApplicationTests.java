af76f1e778311e9fb63fb32f8e93e835c93330ea
seonjuleee <nlsj0820@naver.com>
2023-01-28 09:46:03 +0900

    Initialize project

---
+++ src/test/java/com/seonjuleee/querydslstudy/QuerydslStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seonjuleee.querydslstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
